fa32282d4e2d581a0d5cd7932a3c80a179908129
bill shampton <bill@shampton.net>
2013-03-28 13:43:35 -0500

    adding sinatra basic auth lib

--- routes/init.rb
+++ routes/init.rb
@@ -1,2 +1,12 @@
 # encoding: utf-8
+
+# uncomment to enable basic auth
+#
+# require_relative '../lib/sinatra/basic_auth'
+# basic_auth do
+#   realm 'USCC RETAIL VOUCHER'
+#   username ENV['BASIC_USER']
+#   password ENV['BASIC_PW']
+# end
+
 require_relative 'main'
